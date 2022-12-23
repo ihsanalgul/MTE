9b143c3e63d5d5c31a499a3ecad6ed1586ba0792
İhsan Algül <algulihsan@hotmail.com>
2022-12-23 20:04:15 +0100

    php mailer add

--- sendemail.php
+++ sendemail.php
@@ -1,12 +1,16 @@
 <?php
 
-error_reporting(-1);
-ini_set('display_errors', 'On');
-set_error_handler("var_dump");
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
 
 // Define some constants
-define( "RECIPIENT_NAME", "You are the Boss" );
+define( "RECIPIENT_NAME", "info" );
 define( "RECIPIENT_EMAIL", "info@medicalteameemsdelta.com" );
+
+require_once('../PHPMailer/src/Exception.php');
+require_once('../PHPMailer/src/PHPMailer.php');
+require_once('../PHPMailer/src/SMTP.php');
 
 
 // Read the form values
@@ -17,20 +21,59 @@ $senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/"
 $subject = isset( $_POST['subject'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['subject'] ) : "";
 $message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
 
+try {
+
 // If all values exist, send the email
 if ( $senderName && $senderEmail && $senderPhone && $subject && $message) {
   $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
   $headers = "From: " . $senderName . "";
   $msgBody = " Email: ". $senderEmail .  " Phone: ". $senderPhone . " Subject: ". $subject .  " Message: " . $message . "";
-  $success = mail( $recipient, $headers, $msgBody );
+
+// changes start
+
+$mail = new PHPMailer(true);
+$mail->SMTPDebug = 0;   
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'ssl';
+// $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;    
+$mail->SMTPOptions = array(
+                     'ssl' => array(
+                     'verify_peer' => false,
+                     'verify_peer_name' => false,
+                     'allow_self_signed' => true)
+                     );
+ $mail->Host = 'medicalteameemsdelta.com';
+ $mail->Port = 465;
+ $mail->Username = 'info@medicalteameemsdelta.com';
+ $mail->Password = 'Qwerty.123+';
+ $mail->setFrom($senderEmail);
+
+ $mail->Subject = $headers;
+ $mail->Body = $msgBody;
+ $mail->CharSet = 'UTF-8';
+ $mail->addAddress(RECIPIENT_EMAIL);
+ 
+ $mail->send();
+ 
+ //changes finish
+
+
+  // $success = mail( $recipient, $headers, $msgBody );
+
+  
+echo "Mail has been sent successfully!";
+echo "<script>alert('Your message has been sucessfully submitted Thanks. 🙂');</script>";
+echo "<script>document.location.href='index.html'</script>";
 }
 
-if ($success) {
-  echo "<script>alert('Your message has been sucessfully submitted Thanks. 🙂');</script>";
-  echo "<script>document.location.href='index.html'</script>";
-}else {
+} catch (Exception $e) {
+  echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
   echo "<script>alert('Mail was not Send');</script>";
   echo "<script>document.location.href='contact.html'</script>";
 }
+
+
+
 
 ?>
